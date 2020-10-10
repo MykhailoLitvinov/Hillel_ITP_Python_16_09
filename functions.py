20774e214de7177bbceb4f03bf815c43d27d3737
Володимир Зонтов <vladimir.zontov@gmail.com>
2020-10-10 18:59:56 +0300

    add lesson8

--- functions.py
+++ functions.py
@@ -1,4 +1,31 @@
-import random
+from random import randint
 
-def get_rand():
-    pass
+
+def get_rand_0_255():
+    return randint(0, 255)
+
+
+def get_ip():
+    ip_parts = [str(get_rand_0_255()) for _ in range(4)]
+    return ".".join(ip_parts)
+
+
+def sort_ip_key(ip):
+    ip_parts = ip.split(".")
+    key_list = []
+    for part in ip_parts:
+        key_list.append(int(part))
+    return key_list
+    # return [int(part) for part in ip.split(".")]
+
+
+def generate_list_ip_address(number: int, repeat=True, sort=False) -> list:
+    ip_list = []
+    for _ in range(number):
+        ip_list.append(get_ip())
+    if not repeat:
+        ip_list = list(set(ip_list))
+    if sort:
+        ip_list.sort(key=sort_ip_key)
+    return ip_list
+
--- lesson8.py
+++ lesson8.py
@@ -1,3 +1,22 @@
-from functions import get_rand
+from functions import generate_list_ip_address
 
-get_rand()
+number = 3
+mask = "xx.xx.xx.x"
+ip_list = generate_list_ip_address(number, repeat=False, sort=True)
+
+print(ip_list)
+
+open_file = open("Homeworks/lesson7.txt", "r")
+for line in open_file.readlines():
+    print("********")
+    print(line)
+    print("********")
+open_file.close()
+
+data = []
+with open("sample.txt", "r") as open_file:
+    # data = [line.strip() for line in open_file.readlines()]
+    for line in open_file.readlines():
+        data.append(line.strip())
+
+print(data)
